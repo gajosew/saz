ff6f99f30972ce3574f16f17128a48e689f024d9
sgajek <sgajek@HVBYWOW01.bsb.com.pl>
2016-02-11 13:52:28 +0100

    fggf

--- auto/seconlogin.php
+++ auto/seconlogin.php
@@ -12,7 +12,7 @@ if ($conn->connect_error) die($conn->connect_error);
 	$manager_dn = $_SESSION['manager_dn'];
 	$mail_dn = $_SESSION['mail_dn'];
 	
-	$query = "INSERT INTO usery VALUES" .
+	$query = "INSERT IGNORE INTO usery VALUES" .
 "(NULL,'$login_dn', '$cn_dn', '$mail_dn', '$manager_dn', '$department_dn','0')";
 $result = $conn->query($query);
 header('Location:index.php');
