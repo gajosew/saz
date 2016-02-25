2c771aee133340a978e3b622d3c3b5973dfaed65
sgajek <sgajek@HVBYWOW01.bsb.com.pl>
2016-02-25 12:38:12 +0100

    fggf

---
+++ auto/pag/connect.php
@@ -0,0 +1,16 @@
+<?php
+  //definicja stałych
+  define('SQL_HOST','localhost');
+  define('SQL_USER', 'root');
+  define('SQL_PASS', 'qaz123WSX');//hasło admin1 było by już ekstrawagancją ;)
+  define('SQL_DB', 'rejestr');//tak sobie nazwałem moją bazę
+  
+  //połączenie
+  $conn = mysql_connect(SQL_HOST, SQL_USER, SQL_PASS) or die ("Nie udało sie połączyć z bazą danych MySQL " . mysql_error());
+  mysql_select_db(SQL_DB, $conn);
+  
+  //informacje do poprawnego kodowania
+  mysql_query("SET NAMES utf8");
+  mysql_query("SET CHARACTER SET utf8");
+  mysql_query("SET collation_connection = utf8_general_ci");
+?>
